8f307a888a3037ba54880e90303bd785c1a0d32d
Olivier Lacan <olivier.lacan@gmail.com>
2010-11-24 18:17:04 -0500

    Added some validation.

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -1,3 +1,10 @@
 class Degree < ActiveRecord::Base
   has_many :students
+  
+  # Validations
+  validates :name,
+            :presence => true
+  validates :initials,
+            :presence => true
+            
 end
