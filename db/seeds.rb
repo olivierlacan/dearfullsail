494b158a747937369fafae92f8c6873eebbdd37f
Olivier Lacan <olivier.lacan@gmail.com>
2010-11-30 01:01:24 -0500

    Added seed date for Degrees in the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+degrees = Degree.create([ { :initials => 'CA', :name => 'Computer Animation' },
+                          { :initials => 'DAD', :name => 'Digital Arts and Design'},
+                          { :initials => 'EB', :name => 'Entertainement Business' },
+                          { :initials => 'F', :name => 'Film' },
+                          { :initials => 'GA', :name => 'Game Art'},
+                          { :initials => 'GDev', :name => 'Game Development'},
+                          { :initials => 'MB', :name => 'Music Business' },
+                          { :initials => 'RA', :name => 'Recording Arts' },
+                          { :initials => 'RE', :name => 'Recording Engineering' },
+                          { :initials => 'SP', :name => 'Show Production'}, 
+                          { :initials => 'WDD', :name => 'Web Design and Development'}
+                        ])
