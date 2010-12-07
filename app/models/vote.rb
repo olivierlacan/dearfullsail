63633129c60ef242a4c4677f1c7eba52ffe53ebb
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-07 02:05:18 -0500

    Changed has_one :message to belongs_to :message, because it actually makes sense. Commented out validation that was a wee bit too early.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,12 @@
 class Vote < ActiveRecord::Base
   belongs_to :student
-  has_one :message
+  belongs_to :message
+  
+  # Validation
+  validates :message_id,
+            :presence => true
+  # validates :positive
+  #             :presence => true
+  # validates :student_id,
+  #           :presence => true
 end
