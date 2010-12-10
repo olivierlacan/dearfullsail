c0f1eaa025c966b9c35075a7079241e5670d5039
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-10 15:39:18 -0500

    New student model with Devise for authentication, etc.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,18 @@
 class Student < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :recoverable, 
+          :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :first_name, :last_name, :bio, :date, 
+                  :email, :password, :password_confirmation, :remember_me
   belongs_to :degree
   has_many :messages
   has_many :votes
   
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
-  
   # Validations
-  validates :first_name,
-            :presence => true
-  validates :first_name,
+  validates :first_name, :last_name, :email, :date,
             :presence => true
             
   def to_s
