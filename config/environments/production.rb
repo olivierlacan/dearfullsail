5184f02d2aea24afbf0c59fb7faa77d2b879bf56
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-14 05:51:56 -0500

    Added default_url_options for production on Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Dearfullsail::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'dearfullsail.heroku.com' }
 end
