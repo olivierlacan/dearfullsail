80dbcd738082740f812def8816aa8c023a4502d5
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-14 14:09:03 -0500

    Now only submitting votes if the a current_student exists to avoid errors, outputting and error message when none is found.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -22,24 +22,31 @@ class VotesController < ApplicationController
 
   def create
     @message = Message.find(params[:message_id])
-    if @vote = @message.votes.find_by_student_id(current_student.id)
-      @vote.charge = params[:charge] || 1
-    else
-      @vote = @message.votes.create(:student_id => current_student.id, :charge => params[:charge] || 1)
+    
+    if current_student
+      if @vote = @message.votes.find_by_student_id(current_student.id)
+        @vote.charge = params[:charge] || 1
+      else
+        @vote = @message.votes.create(:student_id => current_student.id, :charge => params[:charge] || 1)
+      end
     end
     
     respond_to do |format|
-      if @vote.save
-        # Caching the vote count inside the message vote_count
-        @message.vote_count = @message.votes.vote_count
-        @message.save
+      if current_student 
+        if @vote.save
+          # Caching the vote count inside the message vote_count
+          @message.vote_count = @message.votes.vote_count
+          @message.save
         
-        format.html { redirect_to(messages_path, :notice => 'Vote was successfully created.') }
-        format.xml  { render :xml => @vote, :status => :created, :location => @vote }
-      else  
-        format.html { redirect_to(messages_path, :notice => 'Nope buddy, you already voted.' ) }
-        #format.html { render :action => "new" }
-        format.xml  { render :xml => @vote.errors, :status => :unprocessable_entity }
+          format.html { redirect_to(messages_path, :notice => 'Vote was successfully created.') }
+          format.xml  { render :xml => @vote, :status => :created, :location => @vote }
+        else  
+          format.html { redirect_to(messages_path, :notice => 'Nope buddy, you already voted.' ) }
+          #format.html { render :action => "new" }
+          format.xml  { render :xml => @vote.errors, :status => :unprocessable_entity }
+        end
+      else
+        format.html { redirect_to(messages_path, :notice => 'You need to login to vote.') }
       end
     end
   end
