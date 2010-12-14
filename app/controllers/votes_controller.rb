362a3dc002fa4995c92b21053afc0e84706b8ab6
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-13 19:28:10 -0600

    The fun stuff. Andrew helped me make sure than students can alter their votes positively or negatively while still only being able to vote (up or down) once per message. Works flawlessly. I had forgotten to pass the student_id while creating the vote since we installed devise on the way to Tulsa.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -22,14 +22,23 @@ class VotesController < ApplicationController
 
   def create
     @message = Message.find(params[:message_id])
-    @vote = @message.votes.create(:charge => params[:charge] || 1)
+    if @vote = @message.votes.find_by_student_id(current_student.id)
+      @vote.charge = params[:charge] || 1
+    else
+      @vote = @message.votes.create(:student_id => current_student.id, :charge => params[:charge] || 1)
+    end
     
     respond_to do |format|
       if @vote.save
+        # Caching the vote count inside the message vote_count
+        @message.vote_count = @message.votes.vote_count
+        @message.save
+        
         format.html { redirect_to(messages_path, :notice => 'Vote was successfully created.') }
         format.xml  { render :xml => @vote, :status => :created, :location => @vote }
-      else
-        format.html { render :action => "new" }
+      else  
+        format.html { redirect_to(messages_path, :notice => 'Nope buddy, you already voted.' ) }
+        #format.html { render :action => "new" }
         format.xml  { render :xml => @vote.errors, :status => :unprocessable_entity }
       end
     end
