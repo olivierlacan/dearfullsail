419a38028e6c51fab8835a1c1cd9748775f764a0
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-15 21:23:10 -0500

    Forgot colons, meh.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ degrees = Degree.create([ { :initials => 'CA', :name => 'Computer Animation' },
                           { :initials => 'SP', :name => 'Show Production'}, 
                           { :initials => 'WDD', :name => 'Web Design and Development'}
                         ])
-students = Student.create([ { :first_name => "Olivier", :last_name => "Lacan", :degree_id => 1, :date => "2009-05-04", :email => "olivierlacan@fullsail.edu", :encrypted_password => "$2a$10$tvx1O9xbFRwLiFRmDhJ/KuY6Z3p/cUZBAfxFmG1m7Eb6...", :password_salt => "$2a$10$Cc0m2IUiB8QBxUigINwUy.", :created_at => "2010-12-10 20:11:41", confirmation_token: "jif45tDcy8ZqLdnD8lUR", confirmation_sent_at: "2010-12-15 19:18:30" }
+students = Student.create([ { :first_name => "Olivier", :last_name => "Lacan", :degree_id => 1, :date => "2009-05-04", :email => "olivierlacan@fullsail.edu", :encrypted_password => "$2a$10$tvx1O9xbFRwLiFRmDhJ/KuY6Z3p/cUZBAfxFmG1m7Eb6...", :password_salt => "$2a$10$Cc0m2IUiB8QBxUigINwUy.", :created_at => "2010-12-10 20:11:41", :confirmation_token: "jif45tDcy8ZqLdnD8lUR", :confirmation_sent_at: "2010-12-15 19:18:30" }
                         ])
                         
 # students = Student.create([ { :first_name => "Andrew", :last_name => "Smith", :degree_id => 1, :date => "2008-08-04", :email => "andrewsmith@fullsail.edu", :encrypted_password => "$2a$10$Cc0m2IUiB8QBxUigINwUy.XEMvSCRkmHcDJ7..PZxPyW...", :password_salt => "$2a$10$Cc0m2IUiB8QBxUigINwUy.", :created_at => "2010-12-10 20:11:41" },
